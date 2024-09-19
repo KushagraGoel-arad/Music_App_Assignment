e3737e8d700bd701431b003d219da1325969a66d
Kushagra Goel <deadxan14@gmail.com>
2024-09-20 01:04:35 +0530

    changing server url for deployment

--- src/app/projects/music/apollo-client.js
+++ src/app/projects/music/apollo-client.js
@@ -1,8 +1,8 @@
-// apollo-client.js
+
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3000/api/music', 
+  uri: 'https://music-app-assignment-zqab.vercel.app/projects/music', 
   cache: new InMemoryCache(),
 });
 
