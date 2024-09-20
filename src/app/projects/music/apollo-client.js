5f5ec1e0d69d626ede2155b4a774703128c0b927
Kushagra Goel <deadxan14@gmail.com>
2024-09-20 10:27:11 +0530

    client url change

--- src/app/projects/music/apollo-client.js
+++ src/app/projects/music/apollo-client.js
@@ -2,7 +2,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://music-app-assignment-zqab.vercel.app/projects/music', 
+  uri: 'api/music', 
   cache: new InMemoryCache(),
 });
 
